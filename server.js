1d5cc4baf24398a1271268806b6e86f55555862f
Nani <navachaitanya1407@gmail.com>
2025-02-27 18:10:40 +0530

    Updated core

--- server.js
+++ server.js
@@ -11,10 +11,14 @@ const app = express();
 app.use(express.json());
 // const cors = require('cors');
 
-app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your frontend URL
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
 
 
 
